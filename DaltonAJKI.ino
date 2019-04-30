463acab7cb12ce6e5110328c6f708a8b59b388fe
Slawomir Krysztowiak <slawomir.krysztowiak@datacave.ch>
2019-04-30 23:03:22 +0200

    Initial version

---
+++ DaltonAJKI.ino
@@ -0,0 +1,57 @@
+// https://github.com/vandalton/DaltonAJKI
+#include <PS4USB.h>
+
+// Atari joystick pinout - seen from the front
+// \1 2 3 4 5/
+//  \6 7 8 9/
+//
+// 1 - up, 2 - down, 3 - left, 4 - right, 6 - fire
+// 7 - 5V, 8 - gnd
+
+#define OUT_UP 2 // connect digital pin 2 of Arduino to pin 1 of joystick port
+#define OUT_DOWN 3
+#define OUT_LEFT 4
+#define OUT_RIGHT 5
+#define OUT_FIRE 6
+
+#define OUT_START A0 // didn't try these buttons yet
+#define OUT_SELECT A1
+#define OUT_OPTION A2
+#define OUT_RESET A3
+
+USB Usb;
+PS4USB PS4(&Usb);
+
+void setup() {
+  pinMode(OUT_UP, OUTPUT);
+  pinMode(OUT_DOWN, OUTPUT);
+  pinMode(OUT_LEFT, OUTPUT);
+  pinMode(OUT_RIGHT, OUTPUT);
+  pinMode(OUT_FIRE, OUTPUT);
+  
+  pinMode(OUT_START, OUTPUT);
+  pinMode(OUT_SELECT, OUTPUT);
+  pinMode(OUT_OPTION, OUTPUT);
+  pinMode(OUT_RESET, OUTPUT);
+  
+  if (Usb.Init() == -1) {
+    while (1); // Halt
+  }
+}
+
+void loop() {
+  Usb.Task();
+
+  if (PS4.connected()) {
+    digitalWrite(OUT_UP, PS4.getButtonPress(UP) || PS4.getAnalogHat(LeftHatY) < 64 ? LOW : HIGH);
+    digitalWrite(OUT_DOWN, PS4.getButtonPress(DOWN) || PS4.getAnalogHat(LeftHatY) > 192 ? LOW : HIGH);
+    digitalWrite(OUT_LEFT, PS4.getButtonPress(LEFT) || PS4.getAnalogHat(LeftHatX) < 64 ? LOW : HIGH);
+    digitalWrite(OUT_RIGHT, PS4.getButtonPress(RIGHT) || PS4.getAnalogHat(LeftHatX) > 192 ? LOW : HIGH);
+    digitalWrite(OUT_FIRE, PS4.getButtonPress(CROSS) ? LOW : HIGH);
+
+    digitalWrite(OUT_START, PS4.getButtonPress(OPTIONS) ? LOW : HIGH);
+    digitalWrite(OUT_SELECT, PS4.getButtonPress(SHARE) ? LOW : HIGH);
+    digitalWrite(OUT_OPTION, PS4.getButtonPress(TOUCHPAD) ? LOW : HIGH);
+    digitalWrite(OUT_RESET, PS4.getButtonPress(OPTIONS) && PS4.getButtonPress(SHARE) ? LOW : HIGH); 
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DaltonAJKI
-Dalton Atari Joystick &amp; Keyboard Interface
+
+## Description
+
+AJKI means Dalton's Atari Joystick and Keyboard Interface
+
+It's a piece of software for Arduino Uno that makes use of MAX3421E based USB Host Shield board and USB Host Shield library. It is an interface to connect an USB joystick, gamepad or keyboard to 8-bit Atari computer.
+
+There is already the project called AKI: http://ftp.pigwa.net/stuff/mirror/www.atari.cuprum.com.pl/aki.htm
+It allows to connect PS2 keyboard to Atari. The idea of DaltonAJKI was to create a modern replacement, because PS2 keyboards are kind of obsolete nowadays. Moreover, I wanted to utilize more than one gamepad button in order to handle "Start", "Select" and "Option" from the gamepad, not from the keyboard.
+
+The initial version (2019-IV-30) can only handle PlayStation 4 gamepad connected by cable. Support for other peripherals is planned.
+
+## Remarks
+
+Use it at your own risk. Remember that there are two versions of USB Host Shield: 3.3V and 5V. Atari works with 5V. This code was tested with 5V-version of USB Host Shield only and won't work with 3.3V.
